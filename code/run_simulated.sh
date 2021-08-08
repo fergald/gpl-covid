d2a07d0f4a0c9d2fa8832e219037598eec950dbc
Fergal Daly <fergald@gmail.com>
2021-08-08 17:57:49 +0900

    Run all countries

--- code/run_simulated.sh
+++ code/run_simulated.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -xe
 
-for npi in pck_social_distance school_closure_popw national_lockdown; do 
-    code/statab.sh do ./code/models/alt_growth_rates/FRA_adm1-$npi.do
+for f in $(ls code/models/alt_growth_rates/???_adm?-*.do); do
+    code/statab.sh do "$f"
 done
