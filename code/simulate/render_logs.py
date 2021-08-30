8eb15ef22304b89d408687fdbb0e9e508d7f0079
Fergal Daly <fergald@gmail.com>
2021-08-30 22:54:03 +0900

    switch to using extracted sections

--- code/simulate/render_logs.py
+++ code/simulate/render_logs.py
@@ -5,25 +5,34 @@ import re
 import sys
 
 
+def ExtractReghdfes(lines):
+  section = []
+  in_section = False
+  for line in lines:
+    # Stata logs every command as ". some_command" so lines that start with
+    # ". " indicate a new section. Close off the old section.
+    if in_section and re.search("^\. ", line):
+      yield section
+      section = []
+      in_section = False
+
+    # We only want the reghdfe outputs.
+    if re.search("^\. reghdfe", line):
+      in_section = True
+
+    if in_section:
+      section.append(line)
+
+
 def main(argv):
   for fn in argv[1:]:
     print(f"# {fn}\n")
 
-    in_section = False
-    for line in open(fn):
-      # Stata logs every command as ". some_command" so lines that start with
-      # ". " indicate a new section. Close off the old section.
-      if in_section and re.search("^\. ", line):
-        print("```")
-        in_section = False
-
-      # We only want the reghdfe outputs.
-      if re.search("^\. reghdfe", line):
-        print("```")
-        in_section = True
-
-      if in_section:
+    for section in ExtractReghdfes(open(fn)):
+      print("```")
+      for line in section:
         sys.stdout.write(line)
+      print("```")
 
 
 if __name__ == '__main__':
