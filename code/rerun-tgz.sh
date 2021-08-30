662e907ca14085c046d8247dabc325cc7a5cd47c
Fergal Daly <fergald@gmail.com>
2021-08-30 11:14:03 +0900

    fix rerun

--- code/rerun-tgz.sh
+++ code/rerun-tgz.sh
@@ -1,8 +1,7 @@
 #! /bin/bash -xe
 
 rm *.log || true
-#./code/run_all_countries.sh
-touch sample.log
+./code/run_all_countries.sh
 TS=$(date "+%Y%m%d-%H%M%S")
 TGZ=../gpl-covid-logs-$TS.tgz
 tar -zcvf "$TGZ" *.log
